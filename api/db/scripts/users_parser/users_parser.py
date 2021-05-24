53a359d6d65125d33ceb6195f84981ac4b3b9216
Iván L. Murillo <diskman199@gmail.com>
2021-05-24 08:14:10 -0500

    Parser de usuarios agregado

---
+++ api/db/scripts/users_parser/users_parser.py
@@ -0,0 +1,50 @@
+"""
+Script to parse files and add to the server
+filename of backup is provided by the export
+list from Active Directory
+"""
+
+import random
+
+def backup():
+    filename = "users.txt"
+
+    with open(filename, 'r', encoding = 'utf-8') as file:
+
+        text = file.read()
+        lines = text.split('\n')
+
+        headers = lines[0].split('\t')
+        name_index = headers.index('Name')
+        email_index = headers.index('E-Mail Address')
+
+        users = []
+
+        #Skip first and las entry
+        for entry in lines[1:-1]:
+            user = entry.split('\t')
+
+            user = {
+                'username' : user[email_index].split('@')[0], 
+                'name' : user[name_index],
+                'email' : user[email_index],
+                'password' : str(random.randint(1000000,9999999)),
+            }
+
+            users.append(user)
+
+        query = []
+        for u in users:
+            subquery = f"('{u['username']}','{u['name']}','{u['email']}','{u['password']}')"
+            query.append(subquery)
+
+        query = 'INSERT INTO usuarios(username,nombre,email,password) VALUES ' +',\n'.join(query)
+
+        print(query)
+    
+    with open('query.txt','w') as file:
+        file.write(query)
+
+        
+if __name__ == "__main__":
+    backup()
