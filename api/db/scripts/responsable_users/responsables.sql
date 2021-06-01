37bdf40ff690dc450bcf393f13c58da151407b0c
Iván L. Murillo <diskman199@gmail.com>
2021-06-01 08:33:32 -0500

    Actualizada base de datos con usuarios nuevos

---
+++ api/db/scripts/responsable_users/responsables.sql
@@ -0,0 +1,80 @@
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('almacen',
+'Almacen',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('embarques.almacen',
+'Almacen de Embarques',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('automatizacion',
+'Automatización',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('calidad',
+'Calidad',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('cnc',
+'CNC',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('cold.forging',
+'Cold Forging',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('estampados',
+'Estampados',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('incoming',
+'Incoming',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('laboratorio',
+'Laboratorio',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('metrologia.lab',
+'Laboratorio de Metrologia',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('procesos',
+'Procesos',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('produccion',
+'Produccion',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('supervisores',
+'Supervisores',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('taller',
+'Taller',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('calidad.tecnicos',
+'Tecnicos de Calidad',
+'password','i.lopez@mx.interplex.com','usuario');
+
+INSERT INTO usuarios(username,nombre,password,email,posicion) VALUES 
+('todos',
+'Todos',
+'password','i.lopez@mx.interplex.com','usuario');
+
