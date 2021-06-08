b6ee89552a699bf2e1ee228a24cddfe964ad01bd
Iván L. Murillo <diskman199@gmail.com>
2021-06-08 08:04:53 -0500

    Controlador de listar equipos con parámetros GET

--- api/controllers/devices.controller.js
+++ api/controllers/devices.controller.js
@@ -3,14 +3,28 @@ const Sql = require('../db/sql');
 exports.getDevices = async(req, res) => {
 
     try{
-        let query = `SELECT TOP 50 equipos.id, descripcion, serie, estado, activo, ubicacion, 
-        calibraciones.fecha 
-        FROM calibraciones,equipos 
-        WHERE calibraciones.equipo = equipos.id
-        AND (SELECT MAX(fecha) FROM calibraciones WHERE equipo = equipos.id) = calibraciones.fecha
-        AND equipos.activo = 'Activo'
-        ORDER BY fecha ASC`;
+        let query;
+        if(Sql.hasQuery(req)){
+            console.log(req.query);
+            let filters = Sql.applyFilters(req.query);
+            console.log(filters);
+            query = `SELECT TOP 50 
+            id, serie, descripcion, estado, activo, ubicacion, 
+            ultima, siguiente  
+            FROM equipos 
+            WHERE ${ filters } 
+            ORDER BY siguiente ASC`;
 
+        }else{
+            query = `SELECT TOP 50 
+            id, serie, descripcion, estado, activo, ubicacion, 
+            ultima, siguiente  
+            FROM equipos 
+            WHERE activo = 'Activo'
+            ORDER BY siguiente ASC`;
+
+        }
+        
         let equipos = await Sql.request(query);
 
         res.json({
--- api/db/sql.js
+++ api/db/sql.js
@@ -11,26 +11,55 @@ const config = {
     }
 }
 
-const hasQuery = (req) =>{
+const applyFilters = (obj) => {
+    params = [];
+    Object.keys(obj).forEach(key => {
+        let filter;
+        switch (key) {
+            case 'fromUltima':
+                filter = `ultima >= '${obj[key]}'`
+                break;
+            case 'toUltima':
+                filter = `ultima <= '${obj[key]}'`
+                break;
+            case 'fromSiguiente':
+                filter = `siguiente >= '${obj[key]}'`
+                break;
+            case 'toSiguiente':
+                filter = `siguiente <= '${obj[key]}'`
+                break;
+            default:
+                filter = `${key} LIKE '%${obj[key]}%'`
+        }
+        params.push(filter);
+    });
+
+    if(params.length > 0){
+        return params.join(' AND ');
+    }
+    return '';
+}
+
+const hasQuery = (req) => {
     return Object.keys(req.query).length !== 0;
 }
 
-const parseField = (str)=>{
+const parseField = (str) => {
     return str?.toString().replace(/'/g, "''") || "";
 }
 
-const query = async(query, data) => {
-    let req = getQuery(query,data);
+const query = async (query, data) => {
+    let req = getQuery(query, data);
     return request(req);
 }
 
-const getQuery = async(query, data) => {
+const getQuery = async (query, data) => {
     let columns = [];
     let rows = [];
     if (!Array.isArray(data)) {
         for (var key of Object.keys(data)) {
             columns.push(key);
-            let val = data[key].toString().replace(/'/g,"''");
+            let val = data[key].toString().replace(/'/g, "''");
             rows.push("'" + val + "'");
 
         }
@@ -45,7 +74,7 @@ const getQuery = async(query, data) => {
                 if (index === 0) {
                     columns.push(key);
                 }
-                let data = val[key].toString().replace(/'/g,"''");
+                let data = val[key].toString().replace(/'/g, "''");
                 row.push("'" + data + "'");
             }
             rows.push(row);
@@ -62,7 +91,7 @@ const getQuery = async(query, data) => {
     }
 }
 
-const request = async(query) => {
+const request = async (query) => {
     console.info(query);
     return new Promise((resolve, reject) => {
         new sql.ConnectionPool(config).connect().then(pool => {
@@ -82,5 +111,6 @@ module.exports = {
     query,
     request,
     parseField,
-    hasQuery
+    hasQuery,
+    applyFilters
 };
