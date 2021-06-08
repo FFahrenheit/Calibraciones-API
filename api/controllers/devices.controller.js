06eedfa8b78b32b129e538f54c4edd0a429c8b18
Iván L. Murillo <diskman199@gmail.com>
2021-06-08 10:51:35 -0500

    Servicios de siguientes calibraciones y en proceso de calibración

--- api/controllers/devices.controller.js
+++ api/controllers/devices.controller.js
@@ -1,5 +1,94 @@
 const Sql = require('../db/sql');
 
+exports.getProcessDevices = async(req, res) =>{
+
+    try{
+        let query;
+        if(Sql.hasQuery(req)){
+            console.log(req.query);
+            let filters = Sql.applyFilters(req.query);
+            console.log(filters);
+            query = `SELECT TOP 50 
+            id, serie, descripcion, estado, activo, ubicacion, 
+            ultima, siguiente  
+            FROM equipos 
+            WHERE estado = 'En Proceso de Calibración'
+            OR estado = 'Calibración Pendiente'
+            AND ${ filters } 
+            ORDER BY siguiente DES`;
+
+        }else{
+            query = `SELECT TOP 50 
+            id, serie, descripcion, estado, activo, ubicacion, 
+            ultima, siguiente  
+            FROM equipos 
+            WHERE estado = 'En Proceso de Calibración'
+            OR estado = 'Calibración Pendiente'
+            ORDER BY siguiente DESC`;
+
+        }
+        
+        let equipos = await Sql.request(query);
+
+        res.json({
+            ok: true,
+            equipos
+        });
+    }catch(e){
+        console.log(e);
+        res.status(500).send({
+            ok: false,
+            error: e
+        });
+    }
+}
+
+
+exports.getNextDevices = async(req, res) =>{
+
+    try{
+        let query;
+        if(Sql.hasQuery(req)){
+            console.log(req.query);
+            let filters = Sql.applyFilters(req.query);
+            console.log(filters);
+            query = `SELECT TOP 50 
+            id, serie, descripcion, estado, activo, ubicacion, 
+            ultima, siguiente  
+            FROM equipos 
+            WHERE activo = 'Activo'
+            AND estado != 'En Proceso de Calibración'
+            AND estado != 'Calibración Pendiente'
+            AND ${ filters } 
+            ORDER BY siguiente ASC`;
+
+        }else{
+            query = `SELECT TOP 50 
+            id, serie, descripcion, estado, activo, ubicacion, 
+            ultima, siguiente  
+            FROM equipos 
+            WHERE activo = 'Activo'
+            AND estado != 'En Proceso de Calibración'
+            AND estado != 'Calibración Pendiente'
+            ORDER BY siguiente ASC`;
+
+        }
+        
+        let equipos = await Sql.request(query);
+
+        res.json({
+            ok: true,
+            equipos
+        });
+    }catch(e){
+        console.log(e);
+        res.status(500).send({
+            ok: false,
+            error: e
+        });
+    }
+}
+
 exports.getDevices = async(req, res) => {
 
     try{
--- api/routers/devices.routes.js
+++ api/routers/devices.routes.js
@@ -3,4 +3,10 @@ const Devices = require('../controllers/devices.controller');
 module.exports = (app) => {
     app.route('/devices/all')
     .get(Devices.getDevices);
+
+    app.route('/devices/next')
+    .get(Devices.getNextDevices);
+
+    app.route('/devices/process')
+    .get(Devices.getProcessDevices);
 }
