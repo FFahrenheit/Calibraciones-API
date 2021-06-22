d6be8855d2c12b252e08e1d68ce536f165f1220e
Iván L. Murillo <diskman199@gmail.com>
2021-06-22 12:06:18 -0500

    Controlador subir certificado

--- api/controllers/upload.controller.js
+++ api/controllers/upload.controller.js
@@ -1,15 +1,40 @@
 const Upload = require('../middlewares/upload');
+const Sql = require('../db/sql');
+
+exports.uploadCertificate = async(req, res) =>{
+    try{
+        await Upload.addCertificate(req,res);
+        let file = req.file.path.toString();
+
+        const filename = file.substring(file.indexOf(`${req.params.device}`));
+        let query = `UPDATE calibraciones SET certificado = '${filename}' WHERE id = '${req.params.id}'`;
+        await Sql.request(query);
+
+        res.json({
+            ok: true,
+            filename
+        });
+    }catch(e){
+        console.log(e);
+        res.status(500).send({
+            ok: false,
+            error: e
+        });
+    }
+}
 
 exports.uploadRyr = async(req, res) =>{
     try{
         await Upload.addRyr(req,res);
-        console.log(req.file);
+        let file = req.file.path.toString();
+
+        const filename = file.substring(file.indexOf(`${req.params.device}`));
+        let query = `UPDATE calibraciones SET ryr = '${filename}' WHERE id = '${req.params.id}'`;
+        await Sql.request(query);
 
         res.json({
             ok: true,
-            id: req.params.id,
-            device: req.params.device,
-            file: req.file
+            filename
         });
     }catch(e){
         console.log(e);
--- api/routers/upload.routes.js
+++ api/routers/upload.routes.js
@@ -3,4 +3,7 @@ const Upload = require('../controllers/upload.controller');
 module.exports = (app) => {
     app.route('/upload/ryr/:device/:id')
     .post(Upload.uploadRyr);
+
+    app.route('/upload/certificate/:device/:id')
+    .post(Upload.uploadCertificate);
 }
