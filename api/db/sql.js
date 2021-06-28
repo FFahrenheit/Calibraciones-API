a94a709a024dfa37ee6739a537eaaf877d4f7fd3
Iván L. Murillo <diskman199@gmail.com>
2021-06-28 07:29:18 -0500

    Separado método de filtrado en otro archivo js y agregado filtro de prestadoo

--- api/db/sql.js
+++ api/db/sql.js
@@ -1,4 +1,5 @@
 const sql = require('mssql');
+const Filters = require('./utils/filters');
 
 const config = {
     user: process.env.BD_USER,
@@ -18,45 +19,7 @@ const insertRecordset = async(query)=>{
 }
 
 const applyFilters = (obj) => {
-    params = [];
-    Object.keys(obj).forEach(key => {
-        let filter;
-        switch (key) {
-            case 'prestatario':
-                filter = `prestatario IN (SELECT username FROM usuarios WHERE 
-                nombre LIKE '%${obj[key]}%' OR username LIKE '%${ obj[key] }%')`;
-                break;
-            case 'activo':
-                filter = `activo = '${obj[key]}'`;
-                break;
-            case 'fromUltima':
-                filter = `ultima >= '${obj[key]}'`
-                break;
-            case 'toUltima':
-                filter = `ultima <= '${obj[key]}'`
-                break;
-            case 'fromSiguiente':
-                filter = `siguiente >= '${obj[key]}'`
-                break;
-            case 'toSiguiente':
-                filter = `siguiente <= '${obj[key]}'`
-                break;
-            case 'fromRemaining':
-                filter = `DATEDIFF(day, GETDATE(),siguiente) >= ${obj[key]}`;
-                break;
-            case 'toRemaining':
-                filter = `DATEDIFF(day, GETDATE(),siguiente) <= ${obj[key]}`;
-                break;
-            default:
-                filter = `${key} LIKE '%${obj[key]}%' COLLATE Latin1_General_CI_AI`
-        }
-        params.push(filter);
-    });
-
-    if(params.length > 0){
-        return params.join(' AND ');
-    }
-    return '';
+    return Filters.deviceFilters(obj);
 }
 
 const hasQuery = (req) => {
