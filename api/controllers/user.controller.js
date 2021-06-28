21bc658f15476b7a4f763ed488c439a7ea766632
Iván L. Murillo <diskman199@gmail.com>
2021-06-28 09:58:17 -0500

    Servicios para  actualizar encargados listos

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,8 +1,58 @@
 const Sql = require('../db/sql');
 
+exports.updateManagers = async(req, res) =>{
+    try{
+        let users = req.body.users;
+        
+        users = users.map(u => "'" + u + "'");
+        users = users.toString();
+
+        console.log(users);
+        let query = `UPDATE usuarios SET posicion = 'usuario' WHERE posicion = 'encargado'`;
+ 
+        await Sql.request(query);
+
+        query = `UPDATE usuarios SET posicion = 'encargado' WHERE username IN (${ users })`;
+
+        await Sql.request(query);
+        
+        return res.json({
+            ok: true
+        });
+
+    }catch(e){
+        console.log(e);
+        res.status(500).send({
+            ok : false,
+            error: e
+        });
+    }
+}
+
+exports.getManagers = async(req, res) =>{
+    try{
+        let query = `SELECT username, nombre as name, email 
+        FROM usuarios
+        WHERE posicion != 'usuario'`;
+        let usuarios = await Sql.request(query);
+        
+        return res.json({
+            ok: true, 
+            usuarios
+        });
+
+    }catch(e){
+        console.log(e);
+        res.status(500).send({
+            ok : false,
+            error: e
+        });
+    }
+}
+
 exports.getUsers = async(req, res) =>{
     try{
-        let query = "SELECT username, nombre as name FROM usuarios";
+        let query = "SELECT username, nombre as name, email FROM usuarios";
         let usuarios = await Sql.request(query);
         
         return res.json({
--- api/routers/user.routes.js
+++ api/routers/user.routes.js
@@ -3,4 +3,8 @@ const Users = require('../controllers/user.controller');
 module.exports = (app) =>{
     app.route('/users')
     .get(Users.getUsers);
+
+    app.route('/mandated')
+    .get(Users.getManagers)
+    .put(Users.updateManagers);
 }
