549d219131eeb583a7d5e9df2ee9f683468bad50
Iván L. Murillo <diskman199@gmail.com>
2021-07-20 09:21:05 -0500

    Parser de equipos DUM listo

--- api/db/scripts/Parsers DUM/excel_to_db/excel_to_db.py
+++ api/db/scripts/Parsers DUM/excel_to_db/excel_to_db.py
@@ -16,20 +16,45 @@ def main():
 
                 rows.append(device[id])
                 rows.append(device['Descripción'])
-                rows.append(device['Estado'])
                 rows.append(device['Status'])
+                rows.append(device['Estado'])
                 rows.append(device['Método de verificación'])
                 rows.append(device['Linea del cliente'])
                 rows.append(device['Resolución-'])
                 rows.append(device['Resolución+'])
                 rows.append(device['Error-'] + ' - ' + device['Error+'])
-                rows.append(device['Periodo'])
+                periodo = float(device['Periodo'])
+                periodo = round(periodo/30)
+                rows.append(str(periodo))
 
                 entries.append(rows)
 
     with open('query.txt','w',encoding='utf-8') as result:
         for e in entries:
             result.write(str(e) + '\n')
+
+    with open('query.sql','w',encoding='utf-8') as sql:
+        data = entries
+        total = [data[x:x+100] for x in range(0, len(data), 100)]
+        for entries in total:
+            query = []            
+            for entrie in entries:
+                columns = []
+                for column in entrie:
+                    columns.append(f"'{ column }'")
+                registry = '(' + ','.join(columns) + ')'
+                query.append(registry)
+                
+            sentence = """INSERT INTO equipos(
+    id, descripcion, estado, activo, calibracion,
+    ubicacion, resInf, resSup, error, periodo
+) VALUES """
+
+            sentence += ',\n'.join(query) + ';\n\n'
+
+            sql.write(sentence)
+
+    print('Total inserts : ' + str(len(data)))
 
 if __name__ == '__main__':
     main()
