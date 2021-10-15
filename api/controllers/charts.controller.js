93600143c0c9157930591708b81e42c0cdfed01f
Iván L. Murillo <diskman199@gmail.com>
2021-10-15 10:54:45 -0500

    Agregado controlador para calibraciones hechas
    Agregada columna de total

--- api/controllers/charts.controller.js
+++ api/controllers/charts.controller.js
@@ -1,5 +1,37 @@
 const Sql = require('../db/sql');
 
+exports.getDoneCalibrations = async(req, res) => {
+    try{
+        const query = `;WITH cte AS
+        (
+            SELECT CAST(GETDATE() AS DATE) AS fecha
+            UNION ALL
+            SELECT DATEADD(DAY, -1, fecha)
+            FROM cte
+            WHERE fecha >= GETDATE()-30
+        )
+        SELECT cte.fecha, 
+        (SELECT COUNT(*) FROM calibraciones WHERE CAST(fecha AS DATE) = cte.fecha AND equipo LIKE 'INT%') as equipos,
+        (SELECT COUNT(*) FROM calibraciones WHERE CAST(fecha AS DATE) = cte.fecha AND equipo LIKE 'FIX%') as fixtures,
+        (SELECT COUNT(*) FROM calibraciones WHERE CAST(fecha AS DATE) = cte.fecha AND equipo LIKE 'DUM%') as dummies,
+        (SELECT COUNT(*) FROM calibraciones WHERE CAST(fecha AS DATE) = cte.fecha) as total
+        FROM cte;`;
+
+        let result = await Sql.request(query);
+        
+        return res.json({
+            ok: true,
+            data: result 
+        });
+        
+    }catch(e){
+        console.log(e);
+        res.status(500).send({
+            ok: false,
+            error: e
+        });
+    }
+}
 
 exports.getNextDevices = async(req, res) =>{
     try{
@@ -14,12 +46,11 @@ exports.getNextDevices = async(req, res) =>{
         SELECT cte.fecha, 
         (SELECT COUNT(*) FROM equipos WHERE activo = 'Activo' AND siguiente = cte.fecha AND id LIKE 'INT%') as equipos,
         (SELECT COUNT(*) FROM equipos WHERE activo = 'Activo' AND siguiente = cte.fecha AND id LIKE 'FIX%') as fixtures,
-        (SELECT COUNT(*) FROM equipos WHERE activo = 'Activo' AND siguiente = cte.fecha AND id LIKE 'DUM%') as dummies
+        (SELECT COUNT(*) FROM equipos WHERE activo = 'Activo' AND siguiente = cte.fecha AND id LIKE 'DUM%') as dummies,
+        (SELECT COUNT(*) FROM equipos WHERE activo = 'Activo' AND siguiente = cte.fecha) as total
         FROM cte`;
 
         let result = await Sql.request(query);
-
-        console.log(result);
 
         return res.json({
             ok: true,
--- api/routers/charts.routes.js
+++ api/routers/charts.routes.js
@@ -3,4 +3,7 @@ const Charts = require('../controllers/charts.controller');
 module.exports = (app) =>{
     app.route('/charts/next')
     .get(Charts.getNextDevices);
+
+    app.route('/charts/done')
+    .get(Charts.getDoneCalibrations);
 }
