eb1c7ddbd59bd822ffc145b7041256c3e5e7a32c
Iván L. Murillo <diskman199@gmail.com>
2021-10-22 09:32:54 -0500

    Log para reinicios

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const express = require('express'),
     app = express(),
     port = process.env.PORT || 33001;
 
+const fs = require('fs');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('trust proxy', true);
@@ -28,7 +30,7 @@ const scheduledTasks = require('./api/controllers/tasker.controller');
 app.use((req, res, next) => {
     let ip = req.ip;
     ip = ip.substr(ip.lastIndexOf(':') + 1);
-    console.table([{ Timestamp: new Date().toLocaleString(), Method: req.method, Request: req.originalUrl, Client: ip}]);
+    console.table([{ Timestamp: new Date().toLocaleString(), Method: req.method, Request: req.originalUrl, Client: ip }]);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
@@ -52,4 +54,13 @@ app.listen(port, () => {
     console.clear();
     console.log('\x1b[32m', `Server running in port ${port} on ${process.env.NODE_ENV} mode`);
     scheduledTasks(cron);
+    
+    if (process.env.NODE_ENV == 'production') {
+        fs.appendFile('daemon/calibracionesapi.restarts.log', 
+        new Date() + ': Server restarted\n', (err) => {
+            if(err){
+                console.log("Couldn't update log");
+            }
+        });
+    }
 });
