f7831206dae37b3cd5271e14e84aa6f36700c1f5
Iván L. Murillo <diskman199@gmail.com>
2021-11-05 08:40:13 -0600

    Backups se mantienen por 7 días

--- api/tasks/daily.backup.js
+++ api/tasks/daily.backup.js
@@ -1,20 +1,47 @@
 const Sql = require('../db/sql');
+const fs = require('fs');
 
-exports.dailyBackup = async() =>{
+exports.dailyBackup = async () => {
 
-    return new Promise(async(resolve)=>{
-        try{
-            const route = `C:\\backup\\Calibraciones\\calibraciones.bak`;
+    return new Promise(async (resolve) => {
+        try {
+            const date = new Date();
+
+            const filename = getFilename(date);
+
+            const route = `C:\\backup\\Calibraciones\\${filename}`;
+
             const query = `BACKUP DATABASE calibraciones 
-            TO DISK = '${ route }' WITH INIT`;
-        
+            TO DISK = '${route}' WITH INIT`;
+
             await Sql.request(query);
 
+            //Delete old file
+            date.setDate(date.getDate() - 7);
+
+            const deletePath = `C:\\backup\\Calibraciones\\${getFilename(date)}`;
+            console.log('Delete file: ' + deletePath);
+            fs.unlink(deletePath, err => {
+                if (err) {
+                    resolve(err.code == 'ENOENT');
+                } else {
+                    resolve(true);
+                }
+            });
+
             resolve(true);
-        }catch(e){
+        } catch (e) {
             console.log(e);
             resolve(false);
         }
     });
 
+}
+
+let getFilename = (date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `calibraciones_${year}-${month}-${day}.bak`
 }
